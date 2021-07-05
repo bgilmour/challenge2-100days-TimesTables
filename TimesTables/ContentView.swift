a4b8a6b08323e8aa0edc1d8bab60a43168a58e84
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-05 12:14:55 +0100

    Initial Commit

---
+++ TimesTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimesTables
+//
+//  Created by Bruce Gilmour on 2021-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Bruce Gilmour on 2021-07-05.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
